b47515e30bdf73e62632d5f098f8dc74f94a7550
Aleksandr Shiryaev <alexshir83@gmail.com>
2020-03-03 09:15:59 +0300

    add multiplication_fix

--- calculator.py
+++ calculator.py
@@ -4,3 +4,7 @@ def add(first_term, second_term):
 
 def subtract(first_term, second_term):
     return first_term - second_term
+
+
+def multiply(first_term, second_term):
+    return first_term * second_term
